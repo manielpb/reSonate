8405755ecc76807eca1c518750754173fe08ccfd
maniel <manuelpepr99@icloud.com>
2024-05-08 16:41:33 +0000

    30 steps into project Problem Solution Note

--- script.js
+++ script.js
@@ -36,5 +36,20 @@ const taskObj = {
  date: dateInput.value,
  description: descriptionInput.value
 };
-console.log(taskObj);
+
+if(dataArrIndex === -1){
+    taskData.unshift(taskObj);
+}
+
+taskData.forEach(({id,title,date,description})=>{
+    (tasksContainer.innerHTML += `<div class="task" id="${id}">
+        <p><strong>Title:</strong>${title}<p>
+        <p><strong>Date:</strong>${date}</p>
+        <p><strong>Description:</strong>${description}</p>
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button>
+    </div>`)
 });
+taskForm.classList.toggle('hidden');
+});
+
